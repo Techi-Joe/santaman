75e94fd2d20b8c3b57a40087d0c1beb8129b0991
Techi <techi.jo3@gmail.com>
2020-12-03 17:37:07 -0600

    Santaman runner file

---
+++ santaman_runner.java
@@ -0,0 +1,10 @@
+/*
+Santaman is a text-based game written for the Christmas of 2020.
+Developed by Techi-Joe!
+*/
+
+class santaman_runner {
+  public static void main(String args[]) {
+    
+  }
+}
