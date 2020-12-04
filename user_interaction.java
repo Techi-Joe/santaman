475f0078ec107532cb7ffa7612663f76f4b19563
Techi <techi.jo3@gmail.com>
2020-12-03 18:08:38 -0600

    Varify user inputs and pass them

---
+++ user_interaction.java
@@ -0,0 +1,16 @@
+/*
+This is a public class used for verifying
+user inputs and passing them to the appropiate classes.
+*/
+
+public class user_interaction {
+
+  // define private vars at start
+  // note: all private vars are abbreviated
+  private String usr_in; // var for storing user text input
+
+  // full constructor method
+  public user_interaction()
+
+  //
+}
