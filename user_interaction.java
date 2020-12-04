f8d377abbfae75c9734b48dd3a68075da342cf2e
Techi <techi.jo3@gmail.com>
2020-12-03 20:55:36 -0600

    started work on classes

--- user_interaction.java
+++ user_interaction.java
@@ -8,8 +8,34 @@ public class user_interaction {
   // define private vars at start
   // note: all private vars are abbreviated
   private String usr_in; // var for storing user text input
+  private boolean valid = false; // stores if last input was valid
 
   // input method is a void method that varifies an entered String
   // and saves it to usr_in
-  
+  public void input(String input, int qualifications) {
+    if (valid_in(input, qualifications) == true) {
+      valid = true;
+      usr_in = input;
+    } else {
+      System.out.println("invalid input! Please try again.")
+      valid = false;
+    }
+  }
+
+  // valid_in boolean method returns true if input meets
+  // certain qualifications
+  public boolean valid_in(String input, int qualifications) {
+    // qualifications is represented by an integer that switches
+    // between different scalable checks on proper input
+    switch(qualifications) {
+      case 0:
+        // case 0 is for 
+      default:
+        // default is basic checks for simple y/n statements
+        if ((input.toLowerCase() == y) || (input.toLowerCase() == n)) {
+          return true;
+        }
+    }
+    return false; // returns false if not valid
+  }
 }
